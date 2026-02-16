5c441ede705e0171acbe7e7b7f928dcc3f83346c
Arjun S V <alphasva1998@gmail.com>
2026-02-16 13:44:58 +0530

    increase detection turn count

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 });
 
 // Main endpoint that responds to scam
-app.post("/api/message", async (req, res) => {
+app.post("/api", async (req, res) => {
   try {
     const result = await handleMessage(req.body);
 
